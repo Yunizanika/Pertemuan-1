1496fea3412e8e5e074a4f08973b283544055206
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-14 14:59:10 +0700

    Tipe Data Dengan Menggunakan Data Bilangan Decimal

---
+++ src/main/kotlin/g_tipe_data_bilangan_decimal.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+
+    /*
+    Cobalah untuk memperhatikan hasil presisi digit yang terekam oleh variabel maxFloat yaitu
+    hanya 6 digit (kelebihan digit akan dibulatkan) sedangkan untuk var maxDouble presisi digit
+    mencapai 10 digit. Untuk memberikan nilai sebagai float maka harus memberikan akhiran f atau
+    F pada akhir literal.
+     */
+}
