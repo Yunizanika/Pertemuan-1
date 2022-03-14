bb29174fecf7042b9bcd2636805430a4356af625
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-14 14:41:20 +0700

    Berbagai Tipe Dasar Literal

---
+++ src/main/kotlin/b_literal.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // mendeklarkan variabel
+
+    // literal dengan tipe data integer
+    var intLiteral = 5
+
+    // literal dengan tipe data double,float(bilangan berkoma)
+    var doubleLiteral = .02
+
+    // literal dengan tipe data string
+    var stringLiteral = "Hello"
+
+    // literal dengan tipe data character
+    var charLiteral = '1'
+
+    // literal dengan tipe data boolean
+    var boolLiteral = true
+
+    // mencetak masing masing literal dengan tipe yang berbeda
+    println("Int Literal = $intLiteral")
+    println("Double Literal = $doubleLiteral")
+    println("String Literal = $stringLiteral")
+    println("Char Literal = $charLiteral")
+    println("Boolean Literal = $boolLiteral")
+}
