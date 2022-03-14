7cc1f50427d9faa6dee75cd7e0e6efcdba9c9436
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-14 14:44:03 +0700

    Var Dan Val

---
+++ src/main/kotlin/e_VarDanVal.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // var (nilainya dapat berubah sepanjang program dieksekusi)
+    var varVariabel: Int
+
+    varVariabel = 56
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+
+    varVariabel = 78
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+
+    // val (nilainya tidak bisa berubah dan hanya bisa diberikan nilai sekali saja ketika program dieksekusi)
+    val phi = 3.14
+
+}
