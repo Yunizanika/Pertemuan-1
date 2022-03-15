2f72acc4f807a142dcdbfd84a3bfec3870d95d35
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-15 19:16:35 +0700

    Boolean

---
+++ src/main/kotlin/n_boolean.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+
+
+    // boolean yang memiliki nilai true dan false,
+    // nilai 1 dan 0 tidak berlaku untuk menunjuk nilai pada sebuah variabel bertipe boolean.
+}
