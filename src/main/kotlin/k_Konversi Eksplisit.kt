12939be1e7e6b119eadf96df8f93f7997c165f25
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-15 19:13:46 +0700

    Konversi Eksplisit

---
+++ src/main/kotlin/k_Konversi Eksplisit.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val valInt: Int = 1
+    val valSum: Long = 3L + valInt
+
+    //konversi implisit
+    println("Konversi variabel valInt secara implisit : " + valSum)
+    val valLong: Long = valInt.toLong()
+
+    // konversi eksplisit
+    println("Konversi variabel valInt secara eksplisit : " + valLong)
+
+    /*
+    setiap number di kotlin mendukung konversi secara eksplisit seperti
+    - toByte()
+    - toShort()
+    - toInt()
+    - toLong()
+    - toFloat()
+    - toDouble()
+    -toChar()
+     */
+}
