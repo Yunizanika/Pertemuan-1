18d47ee21844cd0b62710fef59532441223e9fb3
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-15 19:21:08 +0700

    Perintah Masukkan DiKotlin

---
+++ src/main/kotlin/u_Perintah Masukan di Kotlin.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //Parrot program
+    println("Hi I'm Lora, the virtual parrot, and i love to repeat!");
+    println("Type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+
+    /*
+    yaitu sebuah fungsi readline()!! secara default perintak masukkan dikotlin akan mengembalikan nilai string kedalam variabel penampung
+     */
+}
