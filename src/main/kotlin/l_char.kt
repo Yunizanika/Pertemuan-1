91725bd4f51aab9591b1eb5c1a75e045da009237
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-15 19:15:06 +0700

    Character

---
+++ src/main/kotlin/l_char.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var charA : Char = 'A'
+    //println(charA == 65)
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.toLowerCase())
+    val strA: String = charA.toString()
+    println("Kini charA sudah menjadi String = "+strA)
+
+    /*
+    karakter dikotlin degan memberikannsingle quotes pada nilai sebuah variabel.
+    karakter ikotlin tidak bisa diberlakukan secara langsung sebagai number.
+     */
+}
